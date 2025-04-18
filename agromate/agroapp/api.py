2cf62c5bc73b2e1de59ed806ff8664518f0fe6a5
Alexandr Kozhin <alexandr.kozhin@gmail.com>
2025-04-18 14:32:21 +0300

    Revert "Remove backgroundtasks"
    This reverts commit d34d2f2e5ac5cd33da5906f988d1bd7b9288caaf.

--- agromate/agroapp/api.py
+++ agromate/agroapp/api.py
@@ -1,10 +1,11 @@
 import logging
 from zoneinfo import ZoneInfo
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends, BackgroundTasks
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 from config import settings
-from database import get_next_serial_num, async_session
+from database import get_async_session_as_generator, get_next_serial_num
 from entities import ChatMessage
 from models import ChatMessageCreateRequest, ChatMessageCreateResponse, MessageStatus
 from processors import process_message
@@ -17,23 +18,26 @@ router = APIRouter(
 
 
 @router.post("/messages")
-async def create_message(request: ChatMessageCreateRequest):
+async def create_message(
+        request: ChatMessageCreateRequest,
+        background_tasks: BackgroundTasks,
+        session: AsyncSession = Depends(get_async_session_as_generator),
+):
     try:
         logger.info(f"Received message: {request.message_text}")
-        async with async_session() as session:
-            chat_message = ChatMessage(**request.model_dump())
-            chat_message.id = None
-            chat_message.status = MessageStatus.new
-            chat_message.created_at = request.created_at.astimezone(ZoneInfo("Europe/Moscow")).replace(tzinfo=None)
-            if settings.google_drive_folder_dumped:
-                next_serial = await get_next_serial_num(session, request.user_id)
-                chat_message.serial_num = next_serial
-            else:
-                chat_message.serial_num = 0
-            session.add(chat_message)
-            await session.commit()
-            await session.refresh(chat_message)
-        await process_message(chat_message.id)
+        chat_message = ChatMessage(**request.model_dump())
+        chat_message.id = None
+        chat_message.status = MessageStatus.new
+        chat_message.created_at = request.created_at.astimezone(ZoneInfo("Europe/Moscow")).replace(tzinfo=None)
+        if settings.google_drive_folder_dumped:
+            next_serial = await get_next_serial_num(session, request.user_id)
+            chat_message.serial_num = next_serial
+        else:
+            chat_message.serial_num = 0
+        session.add(chat_message)
+        await session.commit()
+        await session.refresh(chat_message)
+        background_tasks.add_task(process_message, background_tasks, chat_message.id)
         return ChatMessageCreateResponse(
             id=chat_message.id,
         )
