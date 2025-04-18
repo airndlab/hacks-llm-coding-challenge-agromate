cd1414ecde0c6872be77c8d42a57ac6cdde4570d
Alexandr Kozhin <alexandr.kozhin@gmail.com>
2025-04-18 21:24:39 +0300

    Add yellow msg again

--- agromate/agroapp/processors.py
+++ agromate/agroapp/processors.py
@@ -85,7 +85,25 @@ async def process_report(chat_message_id: int):
             message_id=chat_message.message_id,
             status=chat_message.status,
         ))
-        if settings.bot_reply_on_failed and chat_message.status == MessageStatus.failed:
+        if settings.bot_reply_on_failed and chat_message.status == MessageStatus.processed:
+            notes = [
+                report.note
+                for report in reports
+                if not all([report.department_id, report.operation_id, report.crop_id]) and report.note
+            ]
+            if len(notes) > 0:
+                await reply_on_message(ChatMessageReplyRequest(
+                    chat_id=chat_message.chat_id,
+                    message_id=chat_message.message_id,
+                    text=(
+                        f"🌾 Нашла коса на камень..."
+                        f"\n\n"
+                        f"При обработке отчёта возникли некоторые неточности."
+                        f"\n\n"
+                        f"{notes}"
+                    )
+                ))
+        elif settings.bot_reply_on_failed and chat_message.status == MessageStatus.failed:
             await reply_on_message(ChatMessageReplyRequest(
                 chat_id=chat_message.chat_id,
                 message_id=chat_message.message_id,
